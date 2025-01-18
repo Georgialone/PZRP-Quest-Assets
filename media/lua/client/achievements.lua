b34b30c25ee9402dd01a8d9fe71f7de1b3f4044e
Qvantumin <aidenofficialchannel@gmail.com>
2025-01-18 21:41:20 +0300

    fixes

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -1,5 +1,5 @@
 require "Scripting/AchievementManager"
 
 if AchievementManager then
-    AchievementManager.add("doctors_orders","achivment_talk_Melanie",Doctors_Orders-1.png, "Doctor's Orders","Rescue Doctor Melanie Ross from the army barracks.")
+    AchievementManager.add("doctors_orders","achivment_talk_Melanie",Doctors_Orders-1.png, "Doctor's Orders","Rescue Doctor Melanie Ross from the army barracks.", false)
 end
