d4d2eb9a878c4ef41c48aa3bc26237e606d1bc14
mrtooxa <mr.tooxa@gmail.com>
2025-01-20 15:43:33 +0300

    Realese achiv1 / change npc models

---
+++ media/lua/shared/matthew_npc.lua
@@ -0,0 +1,13 @@
+require "Scripting/MFManager"
+
+local matthew_model = {
+    name = "matthew_normal",
+    script = "Base.MaleFolk",
+    beard = "Long",
+    haircut = { "LeftParting", {0.10588235408067703, 0.09019608050584793, 0.08627451211214066} },
+    clothes = {
+        { "Base.Tshirt_DefaultTEXTURE_TINT", {0.3607843220233917, 0.14509804546833038, 0.14509804546833038} },
+        { "Base.Trousers_Denim", 0 },
+        { "Base.Shoes_TrainerTINT", {0.3607843220233917, 0.250980406999588, 0.14509804546833038} }
+    }
+};
---
+++ media/lua/shared/melanie_npc.lua
@@ -0,0 +1,15 @@
+require "Scripting/MFManager"
+
+local melanie_model = {
+    name = "melanie_normal",
+    script = "Base.FemaleFolk",
+    haircut = { "Rachel", {0.3450980484485626, 0.2078431397676468, 0.13333334028720856} },
+    clothes = {
+        { "Base.Tshirt_Scrubs", 0 },
+        { "Base.Trousers_Scrubs", 0 },
+        "Base.JacketLong_Doctor",
+        { "Base.Shoes_Random", 0 },
+        { "Base.Glasses_Normal", 0 },
+        "Base.Bag_FannyPackFront"
+    }
+};
