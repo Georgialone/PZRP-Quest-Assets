f648af80c1336739940fdc2631e05ac4e1e10135
Qvantumin <aidenofficialchannel@gmail.com>
2025-01-20 17:18:02 +0300

    add all characters find quests

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -3,5 +3,9 @@ require "Scripting/AchievementManager"
 if AchievementManager then
     AchievementManager.add("doctors_orders", "achivment_talk_Melanie", "doctor.png", "Doctor's Orders", "Rescue Doctor Melanie Ross from the army barracks.", false)
     AchievementManager.add("medal_of_honor", "achivment_talk_Captain", nil, "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
+    AchievementManager.add("missing_in_action", "achivment_talk_Sergeant",nil,"Missing In Action", "Rescue Sergeant Lily Shaw from the military surplus store.", false)
+    AchievementManager.add("faith", "achivment_talk_Nicole", nil, "Faith", "Rescue Nicole Sherman from the March Ridge church.", false)
+    AchievementManager.add("distress_call", "achivment_talk_Axel", nil, "Distress Call", "Rescue Axel Sanderson from the Muldraugh industrial district mechanic shop.", false)
+    AchievementManager.add("happy_hour", "achivment_talk_Jack", nil, "Happy Hour", "Rescue Jack Johnson from the Muldraugh tavern.Rescue Jack Johnson from the Muldraugh tavern.", false)
 end
 
