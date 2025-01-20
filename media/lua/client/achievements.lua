f7f511125c38d1683020ec6071b1fdbe2e2ed054
mrtooxa <mr.tooxa@gmail.com>
2025-01-20 17:15:50 +0300

    add medal of honor.png

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -2,6 +2,6 @@ require "Scripting/AchievementManager"
 
 if AchievementManager then
     AchievementManager.add("doctors_orders", "achivment_talk_Melanie", "doctor.png", "Doctor's Orders", "Rescue Doctor Melanie Ross from the army barracks.", false)
-    AchievementManager.add("medal_of_honor", "achivment_talk_Captain", nil, "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
+    AchievementManager.add("medal_of_honor", "achivment_talk_Captain", "medal_of_honor.png", "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
 end
 
