469cef89d840a8687d3fa309e96ac59566ce5932
mrtooxa <mr.tooxa@gmail.com>
2025-01-20 18:16:32 +0300

    add all achievements(png,lua settings)

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -2,10 +2,10 @@ require "Scripting/AchievementManager"
 
 if AchievementManager then
     AchievementManager.add("doctors_orders", "achivment_talk_Melanie", "doctor.png", "Doctor's Orders", "Rescue Doctor Melanie Ross from the army barracks.", false)
-    AchievementManager.add("medal_of_honor", "achivment_talk_Captain", nil, "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
-    AchievementManager.add("missing_in_action", "achivment_talk_Sergeant",nil,"Missing In Action", "Rescue Sergeant Lily Shaw from the military surplus store.", false)
-    AchievementManager.add("faith", "achivment_talk_Nicole", nil, "Faith", "Rescue Nicole Sherman from the March Ridge church.", false)
-    AchievementManager.add("distress_call", "achivment_talk_Axel", nil, "Distress Call", "Rescue Axel Sanderson from the Muldraugh industrial district mechanic shop.", false)
-    AchievementManager.add("happy_hour", "achivment_talk_Jack", nil, "Happy Hour", "Rescue Jack Johnson from the Muldraugh tavern.Rescue Jack Johnson from the Muldraugh tavern.", false)
+    AchievementManager.add("medal_of_honor", "achivment_talk_Captain", "medal_of_honor.png", "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
+    AchievementManager.add("missing_in_action", "achivment_talk_Sergeant","missing_in_action.png","Missing In Action", "Rescue Sergeant Lily Shaw from the military surplus store.", false)
+    AchievementManager.add("faith", "achivment_talk_Nicole", "faith.png", "Faith", "Rescue Nicole Sherman from the March Ridge church.", false)
+    AchievementManager.add("distress_call", "achivment_talk_Axel", "distress_call.png", "Distress Call", "Rescue Axel Sanderson from the Muldraugh industrial district mechanic shop.", false)
+    AchievementManager.add("happy_hour", "achivment_talk_Jack", "happy_hour.png", "Happy Hour", "Rescue Jack Johnson from the Muldraugh tavern.Rescue Jack Johnson from the Muldraugh tavern.", false)
 end
 
