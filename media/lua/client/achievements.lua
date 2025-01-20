993fb64f49073d9538e6159266573a536db512ca
Qvantumin <aidenofficialchannel@gmail.com>
2025-01-20 15:39:35 +0300

    add "Medal of Honor" quest

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -2,4 +2,6 @@ require "Scripting/AchievementManager"
 
 if AchievementManager then
     AchievementManager.add("doctors_orders", "achivment_talk_Melanie", "doctor.png", "Doctor's Orders", "Rescue Doctor Melanie Ross from the army barracks.", false)
+    AchievementManager.add("medal_of_honor", "achivmen_talk_Captain", nil, "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
 end
+
