bf662a046d5704507d486a6dd1bd75c2f153501a
mrtooxa <mr.tooxa@gmail.com>
2025-01-24 17:39:42 +0300

    add sergeant quests.txt/Rulesets

--- media/lua/client/Rulesets.lua
+++ media/lua/client/Rulesets.lua
@@ -21,8 +21,6 @@ local function is_ammo(item)
     return false;
 end
 
-ItemFetcher.add_ruleset("Ammo", is_ammo);
-
 local function canned_food(item)
     local scriptItem = item:getScriptItem();
     if scriptItem then
@@ -33,8 +31,6 @@ local function canned_food(item)
     return false;
 end
 
-ItemFetcher.add_ruleset("CannedFood", canned_food);
-
 local function skill_book(item)
     local item_type = item:getType();
     if item_type ~= "Book" and item_type:starts_with("Book") then
@@ -43,4 +39,23 @@ local function skill_book(item)
     return false;
 end
 
+local function is_holsters(item)
+    local item_type = item:getType();
+    if item_type == 'HolsterSimple' or item_type == 'HolsterDouble' then
+        return true;
+    end
+    return false;
+end
+
+local function is_attachments(item)
+    if item:getScriptItem():getDisplayCategory() == "WeaponPart" then
+        return true;
+    end
+    return false;
+end
+
+ItemFetcher.add_ruleset("Attachments", is_attachments);
+ItemFetcher.add_ruleset("Holsters", is_holsters);
+ItemFetcher.add_ruleset("Ammo", is_ammo);
+ItemFetcher.add_ruleset("CannedFood", canned_food);
 ItemFetcher.add_ruleset("SkillBook", skill_book);
