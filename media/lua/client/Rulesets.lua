6c41279abc882f7029a6cfe7193f1bb1a64f72eb
mrtooxa <mr.tooxa@gmail.com>
2025-01-24 09:59:47 +0300

    add captain/add Translate folder for UI/fix Rulesets

--- media/lua/client/Rulesets.lua
+++ media/lua/client/Rulesets.lua
@@ -1,10 +1,24 @@
 require "Scripting/ItemFetcher"
 
-local function any_ammo(item)
-    if item:getScriptItem():getDisplayCategory() == "Ammo" then
-        return true;
+local ammo_types = { 
+    "308Bullets",
+    "223Bullets",
+    "556Bullets",
+    "Bullets38",
+    "Bullets44",
+    "Bullets45",
+    "Bullets9mm",
+    "ShotgunShells"
+}
+
+local function is_ammo(item) 
+    local item_type = item:getType();
+    for i=1, #ammo_types do
+        if ammo_types[i] == item_type then
+            return true;
+        end
     end
     return false;
 end
 
-ItemFetcher.add_ruleset("Ammo", any_ammo);
+ItemFetcher.add_ruleset("Ammo", is_ammo);
