79ced2767358e9d262122958d7a905de921439d4
Qvantumin <aidenofficialchannel@gmail.com>
2025-01-30 18:24:56 +0300

    Melanie and Sergeant achievments

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -1,11 +1,26 @@
 require "Scripting/AchievementManager"
 
 if AchievementManager then
+    --Matthew Achievments
     AchievementManager.add("doctors_orders", "achivment_talk_Melanie", "doctor.png", "Doctor's Orders", "Rescue Doctor Melanie Ross from the army barracks.", false)
     AchievementManager.add("medal_of_honor", "achivment_talk_Captain", "medal_of_honor.png", "Medal Of Honor", "Rescue Captain Michael Hess from the Muldraugh Gas Station.", false)
     AchievementManager.add("missing_in_action", "achivment_talk_Sergeant","missing_in_action.png","Missing In Action", "Rescue Sergeant Lily Shaw from the military surplus store.", false)
     AchievementManager.add("faith", "achivment_talk_Nicole", "faith.png", "Faith", "Rescue Nicole Sherman from the March Ridge church.", false)
     AchievementManager.add("distress_call", "achivment_talk_Axel", "distress_call.png", "Distress Call", "Rescue Axel Sanderson from the Muldraugh industrial district mechanic shop.", false)
-    AchievementManager.add("happy_hour", "achivment_talk_Jack", "happy_hour.png", "Happy Hour", "Rescue Jack Johnson from the Muldraugh tavern.Rescue Jack Johnson from the Muldraugh tavern.", false)
+    AchievementManager.add("happy_hour", "achivment_talk_Jack", "happy_hour.png", "Happy Hour", "Rescue Jack Johnson from the Muldraugh tavern.", false)
+    --Melanie Achievments
+    AchievementManager.add("triage", "achivment_triage", "Triage.png", "Triage", "Completed the mission 'Triage'", false)
+    AchievementManager.add("quarantine", "achivment_quarantine", "Quarantine.png", "Quarantine", "Completed the mission 'Quarantine'", false)
+    AchievementManager.add("emergency_surgery", "achivment_surgery", "Emergency_Surgery.png", "Emergency Surgery", "Completed the mission 'Emergency Surgery'", false)
+    AchievementManager.add("stressed", "achivment_stressed", "Stressed.png", "Stressed", "Completed the mission 'Stressed'", false)
+    AchievementManager.add("break_a_leg", "achivment_leg", "Break_A_Leg.png", "Break A Leg", "Completed the mission 'Break A Leg'", false)
+    --Sergeant Achievments
+    AchievementManager.add("full_capacity", "achivment_capacity", "FullCapacity.png", "Full Capacity", "Completed the mission 'Full Capacity'", false)
+    AchievementManager.add("vengeance", "achivment_vengeance", "Vengeance.png", "Vengeance", "Completed the mission 'Vengeance'", false)
+    AchievementManager.add("dead_drop", "achivment_drop", "DeadDrop.png", "Dead Drop", "Completed the mission 'Dead Drop'", false)
+    AchievementManager.add("quickdraw", "achivment_quickdraw", "Quickdraw.png", "Quickdraw", "Completed the mission 'Quickdraw'", false)
+    AchievementManager.add("gunsmith", "achivment_gunsmith", "Gunsmith.png", "Gunsmith", "Completed the mission 'Quickdraw'", false)
+    --Captain Achievments
+    
 end
 
