6f78962c007a0089c84b3fb6c294a9fd69e6b925
Qvantumin <aidenofficialchannel@gmail.com>
2025-02-12 22:46:50 +0300

    Complete achievments

--- media/lua/client/achievements.lua
+++ media/lua/client/achievements.lua
@@ -14,6 +14,7 @@ if AchievementManager then
     AchievementManager.add("emergency_surgery", "achivment_surgery", "Emergency_Surgery.png", "Emergency Surgery", "Completed the mission 'Emergency Surgery'", false)
     AchievementManager.add("stressed", "achivment_stressed", "Stressed.png", "Stressed", "Completed the mission 'Stressed'", false)
     AchievementManager.add("break_a_leg", "achivment_leg", "Break_A_Leg.png", "Break A Leg", "Completed the mission 'Break A Leg'", false)
+    AchievementManager.add("wrapped", "achivment_wrapped", "Wrapped.png", "Wrapped", "Completed the mission 'Wrapped'", false)
     --Sergeant Achievments
     AchievementManager.add("full_capacity", "achivment_capacity", "FullCapacity.png", "Full Capacity", "Completed the mission 'Full Capacity'", false)
     AchievementManager.add("vengeance", "achivment_vengeance", "Vengeance.png", "Vengeance", "Completed the mission 'Vengeance'", false)
@@ -21,6 +22,28 @@ if AchievementManager then
     AchievementManager.add("quickdraw", "achivment_quickdraw", "Quickdraw.png", "Quickdraw", "Completed the mission 'Quickdraw'", false)
     AchievementManager.add("gunsmith", "achivment_gunsmith", "Gunsmith.png", "Gunsmith", "Completed the mission 'Quickdraw'", false)
     --Captain Achievments
-    
+    AchievementManager.add("thick_skinned", "achivment_skinned", "Thick Skinned.png", "Thick Skinned", "Completed the mission 'Thick Skinned'", false)
+    AchievementManager.add("hoarder", "achivment_hoarder", "Hoarder.png", "Hoarder", "Completed the mission 'Hoarder'", false)
+    AchievementManager.add("blackout", "achivment_blackout", "Blackout.png", "Blackout", "Completed the mission 'Blackout'", false)
+    AchievementManager.add("detonation_protocol", "achivment_protocol", "Detonation Protocol.png", "Detonation Protocol", "Complete the mission 'Detonation Protocol'", false)
+    AchievementManager.add("reconstruction", "achivment_reconstruction", "Reconstruction.png", "Reconstruction", "Complete the mission 'Reconstruction'", false)
+    --Nicole Achievments
+    AchievementManager.add("sweet_tooth", "achivment_tooth", "Sweet Tooth.png", "Sweet Tooth", "Completed the mission 'Sweet Tooth'", false)
+    AchievementManager.add("stockpile", "achivment_stockpile", "Stockpile.png", "Stockpile", "Completed the mission 'Stockpile'", false)
+    AchievementManager.add("meal_prep", "achivment_prep", "Meal Prep.png", "Meal Prep", "Completed the mission 'Meal Prep'", false)
+    AchievementManager.add("ichthyologist", "achivment_ichthyologist", "Ichthyologist.png", "Ichthyologist", "Completed the mission 'Ichthyologist'", false)
+    AchievementManager.add("cargo", "achivment_cargo", "Cargo.png", "Cargo", "Completed the mission Cargo'", false)
+    --Axel Achievments
+    AchievementManager.add("tools_of_the_trade", "achivment_trade", "Tools Of The Trade.png", "Tools Of The Trade", "Completed the mission 'Tools Of The Trade'", false)
+    AchievementManager.add("scrapped", "achivment_scrapped", "Scrapped.png", "Scrapped", "Completed the mission 'Scrapped'", false)
+    AchievementManager.add("engine_trouble", "achivment_trouble", "Engine Trouble.png", "Engine Trouble", "Completed the mission 'Engine Trouble'", false)
+    AchievementManager.add("when_sparks_fly", "achivment_fly", "When Sparks Fly.png", "When Sparks Fly", "Completed the mission 'When Sparks Fly'", false)
+    AchievementManager.add("leftovers", "achivment_leftovers", "Leftovers.png", "Leftovers", "Completed the mission 'Leftovers'", false)
+    --Jack Achievments
+    AchievementManager.add("archivist", "achivment_archivist", "Archivist.png", "Archivist", "Completed the mission 'Archivist'", false)
+    AchievementManager.add("well_informed", "achivment_informed", "Well Informed.png", "Well Informed", "Completed the mission 'Well Informed'", false)
+    AchievementManager.add("lost", "achivment_lost", "Lost.png", "Lost", "Completed the mission 'Lost'", false)
+    AchievementManager.add("stitched", "achivment_up", "Stitched Up.png", "Stitched Up", "Completed the mission 'Stitched Up'", false)
+    AchievementManager.add("forced_entry", "achivment_entry", "Forced Entry.png", "Forced Entry", "Completed the mission 'Forced Entry'", false)
 end
 
